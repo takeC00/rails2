87dd0250fbd14667d32d5a089dbb0c0db36da621
服部剛士 <take.614@icloud.com>
2022-04-29 08:49:13 +0900

    ユーザー情報更新機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
   end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @user = User.all
   end
-  def profile
-  end
+
   def show
-    @user = User.find(params[:id])
+    @user = current_user
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def profile
   end
 end
