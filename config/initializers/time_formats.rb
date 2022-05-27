c87a3d510aa5d5336bc03cb3a0d8de449f034223
服部剛士 <take.614@icloud.com>
2022-05-27 11:47:51 +0900

    gitignore完成

--- config/initializers/time_formats.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-
-Time::DATE_FORMATS[:datetime_jp] = '%Y年 %m月%d日'
--- db/migrate/20220426232508_add_avatar_to_rooms.rb
+++
@@ -1,5 +0,0 @@
-class AddAvatarToRooms < ActiveRecord::Migration[6.1]
-  def change
-    add_column :rooms, :avatar, :string
-  end
-end
